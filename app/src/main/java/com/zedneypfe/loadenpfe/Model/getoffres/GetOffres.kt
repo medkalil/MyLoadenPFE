27e471ac7554cf39f67fbd35c4b4bcc3412be5e6
medkalil <61290350+medkalil@users.noreply.github.com>
2020-06-23 17:26:04 +0100

    1st commit

---
+++ app/src/main/java/com/zedneypfe/loadenpfe/Model/getoffres/GetOffres.kt
@@ -0,0 +1,6 @@
+package com.zedneypfe.loadenpfe.Model.getoffres
+
+data class GetOffres(
+    val result: Result,
+    val time: Time
+)
---
+++ app/src/main/java/com/zedneypfe/loadenpfe/Model/getoffres/Time.kt
@@ -0,0 +1,10 @@
+package com.zedneypfe.loadenpfe.Model.getoffres
+
+data class Time(
+    val date_finish: String,
+    val date_start: String,
+    val duration: Double,
+    val finish: Double,
+    val processing: Double,
+    val start: Double
+)
